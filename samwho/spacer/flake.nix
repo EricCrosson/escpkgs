f2ab85ebf36d8fbdab7f00273e5355eca3b0fc6d
Eric Crosson <eric.s.crosson@utexas.edu>
2023-07-09 11:05:36 -0500

    feat: add spacer

---
+++ samwho/spacer/flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "ast-grep";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    forEachSystem = nixpkgs.lib.genAttrs [
+      "x86_64-linux"
+      "aarch64-darwin"
+    ];
+  in {
+    packages = forEachSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      spacer = pkgs.rustPlatform.buildRustPackage {
+        name = "spacer";
+        src = pkgs.fetchFromGitHub {
+          owner = "samwho";
+          repo = "spacer";
+          rev = "c5fcae3773da8993e470ad0807649cdab4b186d6";
+          sha256 = "sha256-F2PrBlS9uL6BQtjNNabCpl3ofavsUGlBy/Hotm42Oec=";
+        };
+        cargoSha256 = "sha256-4JCYGSJPZK1geD9Yj0veWseNBbVw2SxbAV8GUGVpn6A=";
+        meta = {
+          description = "CLI tool to insert spacers when command output stops";
+          homepage = "https://github.com/samwho/spacer";
+        };
+      };
+    in {
+      default = spacer;
+    });
+  };
+}
