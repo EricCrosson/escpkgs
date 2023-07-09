a52fc15733ac529d2dab99052dd4411d27b7f7af
Eric Crosson <eric.s.crosson@utexas.edu>
2023-07-09 17:05:32 -0500

    fix(spacer): widen supported systems

--- samwho/spacer/flake.nix
+++ samwho/spacer/flake.nix
@@ -10,8 +10,10 @@
     nixpkgs,
   }: let
     forEachSystem = nixpkgs.lib.genAttrs [
-      "x86_64-linux"
       "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
     ];
   in {
     packages = forEachSystem (system: let
